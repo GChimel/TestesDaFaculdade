0054db91c4c733a2c6350ae00e1db1dfa30bda94
GChimel <gustavochimel12@gmail.com>
2022-06-20 17:12:35 -0300

    primeiros tralhos em C

---
+++ DiaDaSemana/dia da semana com case.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+	int numero;
+	printf("Digite em numero o do dia da semana: \n");
+	scanf("%d", &numero);
+	switch(numero){
+		case 1: printf("Domingo. \n");
+			break;
+		case 2: printf("Segunda. \n");
+			break;
+		case 3: printf("Terça. \n");
+			break;
+		case 4: printf("Quarta. \n");
+			break;			
+		case 5: printf("Quinta. \n");
+			break;
+		case 6: printf("Sexta. \n");
+			break;
+		case 7: printf("Sábado. \n");
+			break;
+		default: printf("Os numeros validos sao de 1-7");
+	}
+}
---
+++ PositivoeNegativo/Positivo e Negativo.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+	int valor, resultado;
+	
+	printf("Digite um valor: \n");
+	scanf("%d", &valor);
+	if(valor <= 0){
+		printf("Seu valor e negativo");
+	}
+	else{
+		printf("Seu valor e positivo");
+	}
+}
+
+ 
